289bcfa4d669b04852abd3005bd6dca29d2f36c9
Henry Tseng <henryiis@gmail.com>
2018-05-25 19:47:30 -0400

    Correct spelling

--- packages/eueuq-core/lib/param/index.js
+++ packages/eueuq-core/lib/param/index.js
@@ -8,19 +8,19 @@ const R = require('ramda');
 /**
  *
  * @param  {[type]} data A data Object
- * @return {Object}      A parametrized data Object
+ * @return {Object}      A parameterized data Object
  */
 function params(data) {
   let _permittedList = [];
 
-  function parametrize(nextData) {
+  function parameterize(nextData) {
     let _data = Object.assign({}, nextData);
 
     /**
      * Throws an error when required parameter is not available
      *
      * @param  {String} name A parameter name
-     * @return {Object}      A parametrized data Object
+     * @return {Object}      A parameterized data Object
      */
     Object.defineProperty(_data, 'require', {
       enumerable: false,
@@ -36,20 +36,20 @@ function params(data) {
      * Allows a parameter
      *
      * @param  {String} name A parameter name
-     * @return {Object}      A parametrized data Object
+     * @return {Object}      A parameterized data Object
      */
     Object.defineProperty(_data, 'permit', {
       enumerable: false,
       value: (name) => {
         _permittedList.push(name);
-        return parametrize(R.pick(_permittedList, data));
+        return parameterize(R.pick(_permittedList, data));
       }
     });
-    
+
     return _data;
   }
 
-  return parametrize(data);
+  return parameterize(data);
 }
 
 module.exports = params;

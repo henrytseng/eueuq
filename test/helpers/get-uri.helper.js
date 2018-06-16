00dd28aa9344d226591feafd155a6b4f583711d4
Henry Tseng <henryiis@gmail.com>
2018-06-15 23:58:05 -0400

    Adds comment

--- test/helpers/get-uri.helper.js
+++ test/helpers/get-uri.helper.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * Generates URI with a random port
+ *
+ * @return {String} A URI
+ */
 module.exports = function() {
   let port = Math.floor(Math.random() * 500) + 5000;
   let uri = `eueq://localhost:${port}`;

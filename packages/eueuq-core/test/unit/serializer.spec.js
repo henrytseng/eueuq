7defa7d8ef10cd498eec8e358069660bd8ce2d20
Henry Tseng <henryiis@gmail.com>
2018-06-25 23:08:48 -0400

    Update test to run in desc

--- packages/eueuq-core/test/unit/serializer.spec.js
+++ packages/eueuq-core/test/unit/serializer.spec.js
@@ -2,45 +2,49 @@ const Serializer = require('../../lib/serializer');
 
 function _getRandom() { return Math.round(Math.random() * 0xffffff).toString(16); }
 
-test('get random cipher key fallback default', () => {
-  let serializer = Serializer({ cipherKey: _getRandom() });
-  expect(serializer).toBeTruthy();
-});
+describe('Serializer', () => {
 
-test('encode payload', () => {
-  let serializer = Serializer({ cipherKey: _getRandom() });
-  let data = 'abcdef';
-  expect(serializer.encode(data)).not.toBe(data);
-});
+  test('get random cipher key fallback default', () => {
+    let serializer = Serializer({ cipherKey: _getRandom() });
+    expect(serializer).toBeTruthy();
+  });
 
-test('encodes-decodes payload strings', () => {
-  let serializer = Serializer({ cipherKey: _getRandom() });
-  let data = '123xyz';
-  let encrypted = serializer.encode(data);
-  expect(serializer.decode(encrypted)).toBe(data);
-});
+  test('encode payload', () => {
+    let serializer = Serializer({ cipherKey: _getRandom() });
+    let data = 'abcdef';
+    expect(serializer.encode(data)).not.toBe(data);
+  });
 
-test('encodes-decodes payload numbers', () => {
-  let serializer = Serializer({ cipherKey: _getRandom() });
-  let data = 37128937218;
-  let encrypted = serializer.encode(data);
-  expect(serializer.decode(encrypted)).toBe(data);
-});
+  test('encodes-decodes payload strings', () => {
+    let serializer = Serializer({ cipherKey: _getRandom() });
+    let data = '123xyz';
+    let encrypted = serializer.encode(data);
+    expect(serializer.decode(encrypted)).toBe(data);
+  });
 
-test('encodes-decodes payload booleans', () => {
-  let serializer = Serializer({ cipherKey: _getRandom() });
+  test('encodes-decodes payload numbers', () => {
+    let serializer = Serializer({ cipherKey: _getRandom() });
+    let data = 37128937218;
+    let encrypted = serializer.encode(data);
+    expect(serializer.decode(encrypted)).toBe(data);
+  });
 
-  let testCases = [true, false, 'true', 'false'];
-  testCases
-    .forEach((data) => {
-      let encrypted = serializer.encode(data);
-      expect(serializer.decode(encrypted)).toBe(data);
-    });
-});
+  test('encodes-decodes payload booleans', () => {
+    let serializer = Serializer({ cipherKey: _getRandom() });
 
-test('encodes-decodes payload data Object', () => {
-  let serializer = Serializer({ cipherKey: _getRandom() });
-  let data = { abc: 123, xyz: { def: 'jkl' } };
-  let encrypted = serializer.encode(data);
-  expect(serializer.decode(encrypted)).toEqual(data);
+    let testCases = [true, false, 'true', 'false'];
+    testCases
+      .forEach((data) => {
+        let encrypted = serializer.encode(data);
+        expect(serializer.decode(encrypted)).toBe(data);
+      });
+  });
+
+  test('encodes-decodes payload data Object', () => {
+    let serializer = Serializer({ cipherKey: _getRandom() });
+    let data = { abc: 123, xyz: { def: 'jkl' } };
+    let encrypted = serializer.encode(data);
+    expect(serializer.decode(encrypted)).toEqual(data);
+  });
+
 });

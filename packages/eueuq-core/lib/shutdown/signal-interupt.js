4360f6fa34710c3339cd2807c8c9eb164fb7d35b
Henry Tseng <henryiis@gmail.com>
2018-07-16 14:42:15 -0400

    Use subject for shutdown process

--- packages/eueuq-core/lib/shutdown/signal-interupt.js
+++ packages/eueuq-core/lib/shutdown/signal-interupt.js
@@ -2,30 +2,23 @@
  * Module dependencies
  */
 const { fromEvent } = require('rxjs');
-const { skip, take, merge } = require('rxjs/operators');
+const { Subject } = require('rxjs');
+const { skip, take } = require('rxjs/operators');
 const debug = require('debug')('eueuq:core');
 
 /**
- * Builds a stream for shutdown
+ * Builds a stream for shutdown requests
  *
  * @return {EventEmitter} A broadcaster for shutdown event request
  */
 function SignalInterupt() {
   const _sigint$ = fromEvent(process, 'SIGINT');
   const _unexpected$ = fromEvent(process, 'uncaughtException');
-  const _forcedShutdown$ = _unexpected$.pipe(merge(
-    _sigint$.pipe(skip(1))
-  ));
+  const _shutdownRequest$ = new Subject();
 
   // Attempted shutdown
   _sigint$.subscribe(() => {
     debug('Attempting shutdown');
-  });
-
-  // Forced shutdown
-  _forcedShutdown$.subscribe(() => {
-    debug('Shutdown forced');
-    process.exit(1);
   });
 
   // Attempt graceful shutdown after uncaught errors
@@ -33,7 +26,16 @@ function SignalInterupt() {
     debug('Uncaught exception', err);
   });
 
-  return _forcedShutdown$.pipe(take(1));
+  // Forced shutdown
+  _sigint$.pipe(skip(1), take(1)).subscribe(_shutdownRequest$);
+  _unexpected$.pipe(take(1)).subscribe(_shutdownRequest$);
+  _shutdownRequest$.subscribe(() => {
+    debug('Shutdown forced');
+    process.nextTick(() => process.exit(1));
+  });
+
+  // Only requests before forcing shutdown honored
+  return _sigint$.pipe(take(1));
 }
 
 module.exports = SignalInterupt();
